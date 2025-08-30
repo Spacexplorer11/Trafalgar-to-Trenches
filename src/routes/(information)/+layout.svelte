432c72edd3f94f4b489c8713649658327817a28b
Akaalroop Singh <spacexplorer11@proton.me>
2025-08-30 17:05:29 +0100

    Add mobile responsiveness to the sponsor & BMC buttons on the info pages
    Signed-off-by: Akaalroop Singh <spacexplorer11@proton.me>

--- src/routes/(information)/+layout.svelte
+++ src/routes/(information)/+layout.svelte
@@ -2,14 +2,21 @@
 	import {Home} from "$lib/components";
 </script>
 <Home/>
-<slot/>
-<div style="position: absolute; top: 40px; left: 10px; z-index: 1000;">
-	<iframe height="32" src="https://github.com/sponsors/Spacexplorer11/button" style="border: 0; border-radius: 6px;"
-	        title="Sponsor Spacexplorer11" width="114"></iframe>
-</div>
-<div style="position: absolute; top: 30px; right: 10px; z-index: 1000;">
+<div style="display: flex; justify-content: space-between; padding: 20px 10px; margin-top: 10px;">
+	<iframe
+			height="32"
+			src="https://github.com/sponsors/Spacexplorer11/button"
+			style="border: 0; border-radius: 6px;"
+			title="Sponsor Spacexplorer11"
+			width="114"
+	></iframe>
+
 	<a href="https://www.buymeacoffee.com/spacexplorer11" style="cursor: pointer;" target="_blank">
-		<img alt="Buy Me A Coffee" src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
-		     style="height: 60px !important;width: 217px !important;">
+		<img
+				alt="Buy Me A Coffee"
+				src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+				style="height: 60px; width: 217px;"
+		/>
 	</a>
 </div>
+<slot/>
